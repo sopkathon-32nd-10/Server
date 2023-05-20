e7e0d32b0db0370e48bf818b51929b24daba8c5c
hayoun <fishingest@kookmin.ac.kr>
2023-05-21 00:56:19 +0900

    [feat] RequirementCheck Service 세팅

--- src/main/java/com/sopkaton10/server/service/RequirementCheckService.java
+++ src/main/java/com/sopkaton10/server/service/RequirementCheckService.java
@@ -1,4 +1,23 @@
 package com.sopkaton10.server.service;
 
+import com.sopkaton10.server.controller.dto.response.RequirementCheckResponseDto;
+import com.sopkaton10.server.domain.RequirementCheck;
+import com.sopkaton10.server.repository.RequirementCheckRepository;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class RequirementCheckService {
+    private final RequirementCheckRepository requirementCheckRepository;
+
+    public RequirementCheckResponseDto getRequire(Long userId){
+
+        List<RequirementCheck> checkList=requirementCheckRepository.findAllByUser()
+    }
 }
