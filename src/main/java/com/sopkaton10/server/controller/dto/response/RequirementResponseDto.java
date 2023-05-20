d3e759ea616795aa02d2cc1cb57f578055531169
hayoun <fishingest@kookmin.ac.kr>
2023-05-21 00:16:04 +0900

    [feat] RequirementResponseDto

--- src/main/java/com/sopkaton10/server/controller/dto/response/RequirementResponseDto.java
+++ src/main/java/com/sopkaton10/server/controller/dto/response/RequirementResponseDto.java
@@ -1,2 +1,17 @@
-package com.sopkaton10.server.controller.dto.response;public class RequirementResponseDto {
+package com.sopkaton10.server.controller.dto.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class RequirementResponseDto {
+    private Long id;
+    private String url;
+    private String content;
+
+    public static RequirementResponseDto of(Long id,String url,String content){
+        return new RequirementResponseDto(id,url,content);
+    }
 }
